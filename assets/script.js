2115c99ff3dfa47cf7d596608790788dc60c5c83
Jennifer Besson <jenniferbesson13@hotmail.com>
2023-04-06 18:16:40 +0200

    slider fonctionnel

--- assets/script.js
+++ assets/script.js
@@ -16,32 +16,46 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>",
 	}
 ]
-let dot = document.getElementsByClassName("dot");
-let slideIndex = 0;
+
+let dotIndex = 1;
+let dotIndex2 =0;
 let imageIndex = 1;
-let dotIndex = 0;
 let textIndex = 1;
+let dot = document.getElementsByClassName("dot");
 const BannerImage = document.getElementById("banner-img");
 const BannerTagLine= document.getElementById("banner-text");
 
+console.log(dot) ;
 
 // // evenement sur la fleche droite
 document.querySelector(".arrow_right").addEventListener("click", function() {
-	// changement du bullet point actif au suivant 	
-	dot[dotIndex++].classList.remove("dot_selected");
-	dot[dotIndex].classList.add("dot_selected");	
+	// ajout class selected 		
+	dot[dotIndex++].classList.add("dot_selected");
+	// supprimer class selected 
+	dot[dotIndex2++].classList.remove("dot_selected");	
 	// changement de l'image 		
 	BannerImage.src = "./assets/images/slideshow/"+ slides[imageIndex++].image;	
 	// changement du texte 
 	BannerTagLine.innerHTML = slides[textIndex++].tagLine;
-
-})
+	// Conditions pour boucle infinie 
+	if (imageIndex >= slides.length) {
+		imageIndex = 0;	
+	}
+	if(textIndex >= slides.length) {
+		textIndex = 0;
+	}
+	if(dotIndex >= dot.length) {
+		dotIndex = 0;
+	}
+	if(dotIndex2 == 4) {
+		dotIndex2 = 0;
+	}
+});
 
 // 	// evenement sur la fleche gauche
 document.querySelector(".arrow_left").addEventListener("click", function() {
-	console.log("click")
 	// changement du bullet point actif au suivant 	
-	dot[dotIndex--].classList.remove("dot_selected");
+	dot[dotIndex++].classList.remove("dot_selected");
 	dot[dotIndex].classList.add("dot_selected");	
 	// changement de l'image 		
 	BannerImage.src = "./assets/images/slideshow/"+ slides[imageIndex--].image;	
