f9087f9a64c9daebe5fd6248972d0a60c32d9b78
Jennifer Besson <jenniferbesson13@hotmail.com>
2023-04-07 12:45:42 +0200

    fleche gauche ok

--- assets/script.js
+++ assets/script.js
@@ -17,26 +17,30 @@ const slides = [
 	}
 ]
 
+
 let dotIndex = 1;
-let dotIndex2 =0;
+let dotIndex2 = 0;
+let dotIndex3 = 3;
+
 let imageIndex = 1;
+let imageIndex2 = 3;
+
 let textIndex = 1;
+let textIndex2 = 3;
+
 let dot = document.getElementsByClassName("dot");
 const BannerImage = document.getElementById("banner-img");
 const BannerTagLine= document.getElementById("banner-text");
 
-console.log(dot) ;
-
 // // evenement sur la fleche droite
-document.querySelector(".arrow_right").addEventListener("click", function() {
-	// ajout class selected 		
-	dot[dotIndex++].classList.add("dot_selected");
-	// supprimer class selected 
-	dot[dotIndex2++].classList.remove("dot_selected");	
+document.querySelector(".arrow_right").addEventListener("click", function() {		
 	// changement de l'image 		
 	BannerImage.src = "./assets/images/slideshow/"+ slides[imageIndex++].image;	
 	// changement du texte 
-	BannerTagLine.innerHTML = slides[textIndex++].tagLine;
+	BannerTagLine.innerHTML = slides[textIndex++].tagLine;	
+	// bullet points		
+	dot[dotIndex++].classList.add("dot_selected");
+	dot[dotIndex2++].classList.remove("dot_selected");
 	// Conditions pour boucle infinie 
 	if (imageIndex >= slides.length) {
 		imageIndex = 0;	
@@ -52,16 +56,32 @@ document.querySelector(".arrow_right").addEventListener("click", function() {
 	}
 });
 
+
 // 	// evenement sur la fleche gauche
-document.querySelector(".arrow_left").addEventListener("click", function() {
-	// changement du bullet point actif au suivant 	
-	dot[dotIndex++].classList.remove("dot_selected");
-	dot[dotIndex].classList.add("dot_selected");	
-	// changement de l'image 		
-	BannerImage.src = "./assets/images/slideshow/"+ slides[imageIndex--].image;	
-	// changement du texte 
-	BannerTagLine.innerHTML = slides[textIndex--].tagLine;
+document.querySelector(".arrow_left").addEventListener("click", function() {	
+	// changement de l'image 	
+	BannerImage.src = "./assets/images/slideshow/"+ slides[imageIndex2--].image;	
+	// changement du texte 	
+	BannerTagLine.innerHTML = slides[textIndex2--].tagLine;	
+	// bullet points		
+	dot[dotIndex3--].classList.add("dot_selected");
+	dot[dotIndex2--].classList.remove("dot_selected");
+	// Conditions pour boucle infinie 
+	if (imageIndex2 < 0) {
+		imageIndex2 = 3;	
+	}	
+	if(textIndex2 < 0) {
+		textIndex2 = 3;
+	}
+	if(dotIndex3 < 0) {
+		dotIndex3 = 3;
+	}
+	if(dotIndex2 < 0) {
+		dotIndex2 = 3;
+	}
 })
+
+
 
 
 
